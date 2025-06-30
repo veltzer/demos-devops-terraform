d7b826b27f5e83765c60de4dc57bcea256ffd471
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:16:09 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,13 +1,7 @@
 """ python deps for this project """
 
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
+import config.shared
 
-    "pytest",
-    "pylint",
-    "mypy",
-    "ruff",
-]
-requires = build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = build_requires + test_requires
